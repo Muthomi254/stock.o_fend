b8e576b4ff7b0c07f3cc075efce168d2b3c3d74c
muthomi254 <dmunene202020@gmail.com>
2024-05-15 13:45:51 +0300

    (ft):Setting up footer and its features

---
+++ src/app/(components)/Footer.jsx
@@ -0,0 +1,17 @@
+'use client';
+
+import { Footer } from 'flowbite-react';
+
+export default function Component() {
+  return (
+    <Footer container>
+      <Footer.Copyright href="#" by="Stocko™" year={2024} />
+      <Footer.LinkGroup>
+        <Footer.Link href="#">About</Footer.Link>
+        <Footer.Link href="#">Privacy Policy</Footer.Link>
+        <Footer.Link href="#">Licensing</Footer.Link>
+        <Footer.Link href="#">Contact</Footer.Link>
+      </Footer.LinkGroup>
+    </Footer>
+  );
+}
