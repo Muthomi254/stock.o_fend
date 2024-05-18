5bf4de5584c7f0f2357b68928c6ce43975e4f4dd
muthomi254 <dmunene202020@gmail.com>
2024-05-18 19:31:47 +0300

    (ft):Adding react hot toast to product form  for notifications

---
+++ src/app/(components)/Forms/ProductForm.jsx
@@ -0,0 +1,169 @@
+"use client";
+
+import React, { useState, useRef } from 'react';
+import { useRouter } from 'next/navigation';
+import toast from 'react-hot-toast';
+
+
+export default function ProductForm() {
+
+const router = useRouter();
+const [loading, setLoading] = useState(false);
+const formRef = useRef(null);
+
+const handleSubmit = async (e) => {
+  e.preventDefault();
+  setLoading(true); // Set loading state to true during form submission
+
+  // Use toast.promise to handle loading state and toast notifications
+  await toast.promise(
+    submitForm(), // Example function for form submission
+    {
+      loading: 'Adding ...', // Display loading message
+      success: () => {
+        // router.push('/ProductTable'); // Redirect to ProductTable page on success
+        formRef.current.reset(); // Reset the form upon successful submission
+        return toast('Good Job! Product Added successfully!', {
+          icon: '👏',
+        }); // Return success message
+      },
+      error: (error) => {
+        console.error('Form submission error:', error);
+        return toast.error(
+          'Error while Adding a Product. Please try again later.'
+        ); // Return error message
+      },
+    }
+  );
+
+  setLoading(false); // Set loading state to false after form submission completes
+};
+
+const submitForm = async () => {
+  // Simulate form submission function
+  // Replace this with your actual form submission logic
+  // For demonstration purposes, add a delay using setTimeout
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      // Assume form submission is successful
+      resolve();
+    }, 1000); // Simulate 1 second delay
+  });
+};
+
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-white dark:bg-gray-900">
+      <form
+        className="w-full max-w-lg mx-auto"
+        onSubmit={handleSubmit}
+        ref={formRef}
+      >
+        <div className="relative z-0 w-full mb-5 group">
+          <input
+            type="text"
+            name="stock_name"
+            id="stock_name"
+            className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-green-500 focus:outline-none focus:ring-0 focus:border-green-600 peer"
+            placeholder=" "
+            required
+          />
+          <label
+            htmlFor="stock_name"
+            className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-green-600 peer-focus:dark:text-green-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+          >
+            Stock Name
+          </label>
+        </div>
+        <div className="relative z-0 w-full mb-5 group">
+          <input
+            type="text"
+            name="description"
+            id="description"
+            className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-green-500 focus:outline-none focus:ring-0 focus:border-green-600 peer"
+            placeholder=" "
+            required
+          />
+          <label
+            htmlFor="description"
+            className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-green-600 peer-focus:dark:text-green-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+          >
+            Description
+          </label>
+        </div>
+        <div className="relative z-0 w-full mb-5 group">
+          <input
+            type="text"
+            name="barcode"
+            id="barcode"
+            className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-green-500 focus:outline-none focus:ring-0 focus:border-green-600 peer"
+            placeholder=" "
+            required
+          />
+          <label
+            htmlFor="barcode"
+            className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-green-600 peer-focus:dark:text-green-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+          >
+            Barcode
+          </label>
+        </div>
+        <div className="relative z-0 w-full mb-5 group">
+          <input
+            type="number"
+            name="buying_price"
+            id="buying_price"
+            className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-green-500 focus:outline-none focus:ring-0 focus:border-green-600 peer"
+            placeholder=" "
+            required
+          />
+          <label
+            htmlFor="buying_price"
+            className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-green-600 peer-focus:dark:text-green-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+          >
+            Buying Price
+          </label>
+        </div>
+        <div className="relative z-0 w-full mb-5 group">
+          <input
+            type="number"
+            name="selling_price"
+            id="selling_price"
+            className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-green-500 focus:outline-none focus:ring-0 focus:border-green-600 peer"
+            placeholder=" "
+            required
+          />
+          <label
+            htmlFor="selling_price"
+            className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-green-600 peer-focus:dark:text-green-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+          >
+            Selling Price
+          </label>
+        </div>
+
+        <div className="relative z-0 w-full mb-5 group">
+          <input
+            type="number"
+            name="quantity"
+            id="quantity"
+            className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-green-500 focus:outline-none focus:ring-0 focus:border-green-600 peer"
+            placeholder=" "
+            required
+          />
+          <label
+            htmlFor="quantity"
+            className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-green-600 peer-focus:dark:text-green-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+          >
+            Quantity
+          </label>
+        </div>
+
+        <button
+          type="submit"
+          className="text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800"
+          disabled={loading} // Disable button during form submission
+        >
+          {loading ? 'Submitting...' : 'Submit'}
+        </button>
+      </form>
+    </div>
+  );
+}
