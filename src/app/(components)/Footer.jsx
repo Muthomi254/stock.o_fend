fc1eda8b82afbb8c355db9701f7bc51de62bff42
muthomi254 <dmunene202020@gmail.com>
2024-05-18 19:38:38 +0300

    (chor):adding support pages and renaming cashier and stock item to Businessform and Product form

--- src/app/(components)/Footer.jsx
+++ src/app/(components)/Footer.jsx
@@ -4,9 +4,11 @@ import { Footer } from 'flowbite-react';
 
 export default function Component() {
   return (
-    <Footer  container>
+    <Footer container>
       <Footer.Copyright href="#" by="Stocko™" year={2024} />
       <Footer.LinkGroup>
+        <Footer.Link href="#">Back To Top</Footer.Link>
+
         <Footer.Link href="#">About</Footer.Link>
         <Footer.Link href="#">Privacy Policy</Footer.Link>
         <Footer.Link href="#">Licensing</Footer.Link>
---
+++ src/app/(pages)/(stock)/test/page.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import BusinessForm from '../../(display)/BusinessForm/page';
+import StockItems from '../../../(components)/Forms/ProductForm';
+
+export default function page() {
+  return (
+    <div>
+      <BusinessForm />
+      {/* <StockItems/> */}
+    </div>
+  );
+}
