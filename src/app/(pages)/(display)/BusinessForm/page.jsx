35ef5fd5d427cdd53e7ae94a332e6886de0b7031
muthomi254 <dmunene202020@gmail.com>
2024-05-18 19:33:13 +0300

    (ft):Adding react hot toast to business form

---
+++ src/app/(pages)/(display)/BusinessForm/page.jsx
@@ -0,0 +1,171 @@
+"use client";
+
+import React, {useState} from 'react';
+import { useRouter } from 'next/navigation';
+import toast from 'react-hot-toast';
+
+export default function BusinessForm() {
+  const router = useRouter();
+  const [loading, setLoading] = useState(false);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setLoading(true); // Set loading state to true during form submission
+
+    // Use toast.promise to handle loading state and toast notifications
+    await toast.promise(
+      submitForm(), // Example function for form submission
+      {
+        loading: 'Creating ...', // Display loading message
+        success: () => {
+          router.push('/ProductTable'); // Redirect to ProductTable page on success
+          return toast('Good Job! Business created successfully!', {
+            icon: '👏',
+          }); // Return success message
+        },
+        error: (error) => {
+          console.error('Form submission error:', error);
+          return toast.error(
+            'Error while Creating a Business. Please try again later.'
+          ); // Return error message
+        },
+      }
+    );
+
+    setLoading(false); // Set loading state to false after form submission completes
+  };
+
+
+  const submitForm = async () => {
+    // Simulate form submission function
+    // Replace this with your actual form submission logic
+    // For demonstration purposes, add a delay using setTimeout
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        // Assume form submission is successful
+        resolve();
+      }, 1000); // Simulate 1 second delay
+    });
+  };
+
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-white dark:bg-gray-900">
+      <form className="w-full max-w-lg mx-auto" onSubmit={handleSubmit}>
+        <div className="relative z-0 w-full mb-5 group">
+          <input
+            type="text"
+            name="business_name"
+            id="business_name"
+            className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-green-500 focus:outline-none focus:ring-0 focus:border-green-600 peer"
+            placeholder=" "
+            required
+          />
+          <label
+            htmlFor="business_name"
+            className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-green-600 peer-focus:dark:text-green-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+          >
+            Business Name
+          </label>
+        </div>
+        <div className="grid md:grid-cols-2 md:gap-6">
+          <div className="relative z-0 w-full mb-5 group">
+            <input
+              type="tel"
+              name="mobile_no"
+              id="mobile_no"
+              className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-green-500 focus:outline-none focus:ring-0 focus:border-green-600 peer"
+              placeholder=" "
+              required
+            />
+            <label
+              htmlFor="mobile_no"
+              className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-green-600 peer-focus:dark:text-green-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+            >
+              Mobile Number
+            </label>
+          </div>
+          <div className="relative z-0 w-full mb-5 group">
+            <input
+              type="email"
+              name="email"
+              id="email"
+              className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-green-500 focus:outline-none focus:ring-0 focus:border-green-600 peer"
+              placeholder=" "
+              required
+            />
+            <label
+              htmlFor="email"
+              className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-green-600 peer-focus:dark:text-green-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+            >
+              Email
+            </label>
+          </div>
+        </div>
+        <div className="relative z-0 w-full mb-5 group">
+          <input
+            type="text"
+            name="address"
+            id="address"
+            className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-green-500 focus:outline-none focus:ring-0 focus:border-green-600 peer"
+            placeholder=" "
+            required
+          />
+          <label
+            htmlFor="address"
+            className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-green-600 peer-focus:dark:text-green-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+          >
+            Address
+          </label>
+        </div>
+        <div className="relative z-0 w-full mb-5 group">
+          <input
+            type="file"
+            name="logo"
+            id="logo"
+            className="block py-2.5 px-0 w-full text-sm text-gray-500 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-500 dark:focus:border-green-500 focus:outline-none focus:ring-0 focus:border-green-600 peer"
+            placeholder=" "
+            required
+          />
+          <label
+            htmlFor="logo"
+            className="peer-focus:font-medium absolute text-sm text-gray-600 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-green-600 peer-focus:dark:text-green-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+          >
+            Upload Your Business Logo
+          </label>
+        </div>
+        <div className="relative z-0 w-full mb-5 group">
+          <textarea
+            name="description"
+            id="description"
+            className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-green-500 focus:outline-none focus:ring-0 focus:border-green-600 peer"
+            placeholder=" "
+            rows="4"
+            required
+          ></textarea>
+          <label
+            htmlFor="description"
+            className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-green-600 peer-focus:dark:text-green-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+          >
+            Business Description
+          </label>
+        </div>
+        <div className="flex justify-between">
+          <button
+            type="submit"
+            className="text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-green-500 dark:hover:bg-green-600 dark:focus:ring-green-800"
+            disabled={loading} // Disable button during form submission
+          >
+            {loading ? 'Submitting...' : 'Submit'}
+          </button>
+          <button
+            type="button"
+            onClick={() => router.back()}
+            className="text-white bg-gray-500 hover:bg-gray-600 focus:ring-4 focus:outline-none focus:ring-gray-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-gray-400 dark:hover:bg-gray-500 dark:focus:ring-gray-700"
+          >
+            Back
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+}
